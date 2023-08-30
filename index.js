c1d9a6c24db09cb15aebd3658cee85208df8f8f0
soumya <soumyadeep184@gmail.com>
2023-08-30 22:13:40 +0530

    table

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const prompt = require("prompt-sync")();
 
-let num = Math.floor(Math.random() * 10);
-let guess = -1;
-let cnt = 1;
+// let num = Math.floor(Math.random() * 10);
+// let guess = -1;
+// let cnt = 1;
 
-while (guess != num) {
-  guess = prompt("Enter a number: ");
+// while (guess != num) {
+//   guess = prompt("Enter a number: ");
 
-  if (guess > num) {
-    console.log("Greater");
-  } else if (guess < num) {
-    console.log("Smaller");
-  } else {
-    console.log("You got it!!");
-    console.log("Your point = ", -cnt);
-  }
+//   if (guess > num) {
+//     console.log("Greater");
+//   } else if (guess < num) {
+//     console.log("Smaller");
+//   } else {
+//     console.log("You got it!!");
+//     console.log("Your point = ", -cnt);
+//   }
 
-  cnt++;
-}
+//   cnt++;
+// }
+
+let obj = {
+  a: 3,
+  b: 6,
+  c: 7
+};
+
+console.table(obj);
